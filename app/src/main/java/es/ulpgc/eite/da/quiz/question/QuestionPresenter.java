64c09b3eb7891d74ad5aeb53af160407f93c263a
Elias <elias.perez102@alu.ulpgc.es>
2022-03-26 16:37:03 +0000

    Array de preguntas con confirmacion de true/false, además se guarda el estado al girar la pantalla question

---
+++ app/src/main/java/es/ulpgc/eite/da/quiz/question/QuestionPresenter.java
@@ -0,0 +1,178 @@
+package es.ulpgc.eite.da.quiz.question;
+
+import android.util.Log;
+
+import java.lang.ref.WeakReference;
+
+import es.ulpgc.eite.da.quiz.app.AppMediator;
+import es.ulpgc.eite.da.quiz.app.CheatToQuestionState;
+import es.ulpgc.eite.da.quiz.app.QuestionToCheatState;
+
+public class QuestionPresenter implements QuestionContract.Presenter {
+
+  public static String TAG = QuestionPresenter.class.getSimpleName();
+
+  private AppMediator mediator;
+  private WeakReference<QuestionContract.View> view;
+  private QuestionState state;
+  private QuestionContract.Model model;
+
+  public QuestionPresenter(AppMediator mediator) {
+    this.mediator = mediator;
+    state = mediator.getQuestionState();
+  }
+
+  @Override
+  public void onStart() {
+    Log.e(TAG, "onStart()");
+
+    // call the model
+    state.question = model.getQuestion();
+    state.option1 = model.getOption1();
+    state.option2 = model.getOption2();
+    state.option3 = model.getOption3();
+
+    // reset state to tests
+    state.answerCheated=false;
+    state.optionClicked = false;
+    state.option = 0;
+
+    // update the view
+    disableNextButton();
+    view.get().resetReply();
+  }
+
+
+  @Override
+  public void onRestart() {
+    Log.e(TAG, "onRestart()");
+
+    //TODO: falta implementacion
+
+    if(state.optionClicked){
+      if(model.isCorrectOption(state.option)){
+        view.get().updateReply(true);
+      }else{
+        view.get().updateReply(false);
+      }
+
+    }else{
+      view.get().resetReply();
+    }
+
+  }
+
+
+  @Override
+  public void onResume() {
+    Log.e(TAG, "onResume()");
+
+    //TODO: falta implementacion
+
+    // use passed state if is necessary
+    CheatToQuestionState savedState = getStateFromCheatScreen();
+    if (savedState != null) {
+      // fetch the model
+      state.answerCheated = savedState.answerCheated;
+      if (state.answerCheated) {
+        if (model.hasQuizFinished()) {
+          state.optionEnabled = false;
+          state.nextEnabled = false;
+        } else {
+          onNextButtonClicked();
+        }
+      }
+    }
+
+    // update the view
+    view.get().displayQuestion(state);
+  }
+
+
+  @Override
+  public void onDestroy() {
+    Log.e(TAG, "onDestroy()");
+  }
+
+  @Override
+  public void onOptionButtonClicked(int option) {
+    Log.e(TAG, "onOptionButtonClicked()");
+
+    //TODO: falta implementacion
+    state.optionClicked = true;
+    boolean isCorrect = model.isCorrectOption(option);
+    if(option == 1){
+      state.option = 1;
+    }else if(option == 2){
+      state.option = 2;
+    }else{
+      state.option = 3;
+    }
+    // una vez se responde se habilita el boton next y sale si es correcto incorrecto.
+    if(isCorrect) {
+      state.cheatEnabled=false;
+    } else {
+      state.cheatEnabled=true;
+    }
+    this.enableNextButton();
+    view.get().updateReply(isCorrect);
+    view.get().displayQuestion(state);
+  }
+  
+  @Override
+  public void onNextButtonClicked() {
+    Log.e(TAG, "onNextButtonClicked()");
+
+    //TODO: falta implementacion
+
+  }
+
+  @Override
+  public void onCheatButtonClicked() {
+    Log.e(TAG, "onCheatButtonClicked()");
+
+    //TODO: falta implementacion
+
+  }
+
+  private void passStateToCheatScreen(QuestionToCheatState state) {
+
+    //TODO: falta implementacion
+    mediator.setQuestionToCheatState(state);
+
+
+  }
+
+  private CheatToQuestionState getStateFromCheatScreen() {
+
+    //TODO: falta implementacion
+
+    return null;
+  }
+
+  private void disableNextButton() {
+    state.optionEnabled=true;
+    state.cheatEnabled=true;
+    state.nextEnabled=false;
+
+  }
+
+  private void enableNextButton() {
+    state.optionEnabled=false;
+
+    if(!model.hasQuizFinished()) {
+      state.nextEnabled=true;
+    }
+  }
+
+  @Override
+  public void injectView(WeakReference<QuestionContract.View> view) {
+    this.view = view;
+  }
+
+  @Override
+  public void injectModel(QuestionContract.Model model) {
+    this.model = model;
+  }
+
+}
---
+++ app/src/main/java/es/ulpgc/eite/da/quiz/question/QuestionState.java
@@ -0,0 +1,10 @@
+package es.ulpgc.eite.da.quiz.question;
+
+public class QuestionState extends QuestionViewModel {
+
+  // put the model state here
+
+  public int quizIndex;
+    public Object questionText;
+  public String resultText;
+}
